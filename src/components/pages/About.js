eddfc9b32ebf5c5832ac08fb793b5df12fd68be5
Gcoder9 <104526581+Gcoder9@users.noreply.github.com>
2022-07-27 10:20:14 -0400

    fixed image in about.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import image from '../images';
+import image from '../images/linkPic.jpg';
 export default function About() {
   return (
     
     <div>
       <h2>About Me</h2>
-      <img src={image} height={100} width={100} />
+      <img src={image} alt= "Me" height={100} width={100} />
       <p>
       Attended UNCC full-stack coding Bootcamp. Developed a strong foundation in software engineering and coding principles across multiple platforms. Able to quickly Learn and master new technologies; successful working in both team and self-directed settings.
       </p>
